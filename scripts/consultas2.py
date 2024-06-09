a028f8b76a1c19e3dbff125504c581b210cc1f36
Francisco Samaniego <141266350+fmasamaniego@users.noreply.github.com>
2024-06-09 11:46:50 -0300

    Update consultas2.py

--- scripts/consultas2.py
+++ scripts/consultas2.py
@@ -1,71 +1,28 @@
-#Para realizar consultas en la base de datos de MongoDB
-
 from pymongo import MongoClient
-import json
 
+client = MongoClient()
 
-# Conexión al servidor local de MongoDB
-client = MongoClient('localhost', 27017)
-
-# Conexión a la base de datos
 db = client['test']
 
-# Conexión a la colección
-collection = db['Muestras_CO2']
+coleccion = db['Muestras_CO2']
 
-#Realizar consultas
+#1- Registros del crecimiento de CO2
 
-#1- Todos los registros de Argentina
-
-documents_arg = collection.find({"country": "Argentina"})
-
-
-#Guardar los datos en un archivo json
-data_arg = []
-for document in documents_arg:
-    document['_id'] = str(document['_id'])
-    data_arg.append(document)
-
-with open('./datos_argentina.json', 'w') as f:
-    json.dump(data_arg, f, indent=4)
-
-#2- Todos los registros de Argentina a partir del año 2000
-documents_arg_2000 = collection.find({"country": "Argentina", "year": {"$gte": "2000"}})
-data_arg_2000 = []
-for document in documents_arg_2000:
-    document['_id'] = str(document['_id'])
-    data_arg_2000.append(document)
-
-with open('./datos_argentina_2000.json', 'w') as f:
-    json.dump(data_arg_2000, f, indent=4)
+consulta1 = coleccion.aggregate([
+    {"$match": {'country':'Argentina', 'year': {'$gte': 2010, '$lte': 2020}}},
+    {"$project": {'_id': 0, 'year': 1, 'co2_growth_prct': 1}}])
 
 
-#3- Paises con mayor emisión de C02 en un año específico
-top_co2_countries = collection.find({"year": "2020", "co2": {"$ne": ""}}).sort("co2", -1).limit(10)
+#2- Emisiones de CO2 por tipo de combustible en 2022
+consulta2 = coleccion.aggregate([
+    {"$match": {'year': 2022}},
+    {"$project": {'coal_co2': 1, 'oil_co2': 1, 'gas_co2': 1, 'cement_co2': 1, 'flaring_co2': 1}}])
 
-data_top_co2_countries_year = []
-for document in top_co2_countries:
-    document['_id'] = str(document['_id'])
-    data_top_co2_countries_year.append(document)
+#3- Paises con mayor consumo energético per cápita y per GDP a partir del año 2000
+consulta3 = coleccion.aggregate([
+    {"$match": {'year': {'$gte':2000}}},
+    {"$group": {'_id': '$country', 'avg_energy_per_gdp': {'$avg': '$energy_per_gdp'}, 'avg_energy_per_capita': {'$avg': '$energy_per_capita'}}},
+    {"$sort": {'avg_energy_per_capita': -1}},
+    {"$limit": 10},
+    {"$project": {'_id': 0, 'country': '$_id', 'avg_energy_per_gdp': 1, 'avg_energy_per_capita': 1}}])
 
-with open('./top_co2_countries_year.json', 'w') as f:
-    json.dump(data_top_co2_countries_year, f, indent=4)
-
-
-#4- Paises con mayor crecimiento de emisión de C02 en un rango de años
-countries_co2_growth = collection.aggregate([
-    {"$match": {"year": {"$gte": "2000"}}},
-    {"$group": {"_id": "$country", "co2_growth": {"$sum": "$co2_growth_abs"}}},
-    {"$sort": {"co2_growth": -1}},
-    {"$limit": 10}
-])
-
-data_top_co2_growth = []
-
-for document in countries_co2_growth:
-    document['_id'] = str(document['_id'])
-    data_top_co2_growth.append(document)
-
-with open('./top_co2_growth.json', 'w') as f:
-    json.dump(data_top_co2_growth, f, indent=4)
- 
